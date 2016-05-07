b1195092a12741fc3605a9f8853764988aa305a7
Yasmine Abulhab <yabulhab@gmail.com>
2016-05-07 16:47:11 -0400

    added ability to search users by name, displays photo and ID, has http request to github API

--- script.js
+++ script.js
@@ -2,6 +2,24 @@
 //initiates the angular app
 var findGitUserApp = angular.module('findGitUserApp', []);
 
-findGitUserApp.controller("SearchCtrl", function($scope){
-	$scope.foo = "bologna"
+findGitUserApp.controller("SearchCtrl", function($scope, $http){
+	$scope.foo = "";
+	$scope.profileData = "";
+	// Simple GET request example:
+	$scope.callHttp = function() {
+		$http({
+		  method: 'GET',
+		  url: 'https://api.github.com/users/' + $scope.foo
+		}).then(function successCallback(response) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+		    $scope.profileData = response.data;
+		    console.log("A response", response);
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		    console.log("we got an error", response);
+		  });
+	}
 });
+
